f7fbd2d6acd43e83fb1b4b38c5751471e6d78bb7
Brent Westbrook <brentrwestbrook@gmail.com>
2022-10-10 13:52:02 -0400

    partial revert of d0882e864, gating dump debug behind env var

--- src/dump.rs
+++ src/dump.rs
@@ -4,6 +4,24 @@ pub(crate) struct Dump {
     pub(crate) max: usize,
 }
 
+use std::sync::Once;
+static mut DEBUG: bool = false;
+static INIT: Once = Once::new();
+
+/// check the `SEMP_DUMP_DEBUG` environment variable on first call and from then
+/// on report whether or not it was set to `1`
+fn is_debug() -> bool {
+    unsafe {
+        INIT.call_once(|| {
+            let v = std::env::var("SEMP_DUMP_DEBUG").unwrap_or_default();
+            if v == "1" {
+                DEBUG = true;
+            }
+        });
+        DEBUG
+    }
+}
+
 impl Dump {
     pub(crate) fn new(size: usize) -> Self {
         Self {
@@ -24,8 +42,16 @@ impl Dump {
     }
 
     fn dump(&mut self) {
+        let now = std::time::Instant::now();
+        let n = self.buf.len();
         for file in &self.buf {
             let _ = std::fs::remove_file(file);
+        }
+        if is_debug() {
+            eprintln!(
+                "finished dumping {n} jobs after {:.1} sec",
+                now.elapsed().as_millis() as f64 / 1e3
+            );
         }
         self.ptr = 0;
     }
